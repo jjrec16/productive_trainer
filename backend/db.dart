75e5a2cd60f510bd2b62718dc6ef01a61dd62561
Yaraslau Yarats <jjrec16@gmail.com>
2025-05-29 01:05:14 +0200

    Poprawiona nazwa bazy na productive_trainer

--- backend/db.dart
+++ backend/db.dart
@@ -2,6 +2,6 @@ final conn = await MySqlConnection.connect(ConnectionSettings(
   host: 'sql.freedb.tech',
   port: 3306,
   user: 'freedb_productive_trainer',
-  password: 'TbZ@CfNgz\$f&9\$D', // ← poprawny string zakończony
+  password: r'TbZ@CfNgz$f&9$D',  // ← RAW STRING: nie musisz uciekać znaków
   db: 'productive_trainer',
 ));
