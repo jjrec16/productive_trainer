dab19d87a724024a88a4d3aa9e17bf544477038b
Yaraslau Yarats <jjrec16@gmail.com>
2025-05-29 02:12:17 +0200

    Poprawka db.dart

--- backend/db.dart
+++ backend/db.dart
@@ -1,12 +1,21 @@
+import 'package:mysql1/mysql1.dart';
+
+Future<MySqlConnection> getConnection() {
+  final settings = ConnectionSettings(
+    host: 'sql.freedb.tech',
+    port: 3306,
+    user: 'freedb_productive_trainer',
+    password: r'TbZ@CfNgz$f&9$D',
+    db: 'productive_trainer',
+  );
+
+  return MySqlConnection.connect(settings);
+}
+
+// Opcjonalnie możesz zachować funkcję main do testów lokalnych:
 void main() async {
   try {
-    final conn = await MySqlConnection.connect(ConnectionSettings(
-      host: 'sql.freedb.tech',
-      port: 3306,
-      user: 'freedb_productive_trainer',
-      password: r'TbZ@CfNgz$f&9$D',
-      db: 'productive_trainer',
-    ));
+    final conn = await getConnection();
     print('✅ Połączono z bazą danych!');
     await conn.close();
   } catch (e) {
